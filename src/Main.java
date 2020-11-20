e0e46a24df209b5f6fe074ee87b3ed004520a879
Olga Nevezina <nevezhinao@yandex.ru>
2020-11-20 18:27:25 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
         int bill = 100;
         int enrollment = 3000;
+        int amount;
         if (enrollment>=1000){
-            int amount = bill + enrollment + enrollment / 100;
-            System.out.println(amount);
+            amount = bill + enrollment + enrollment / 100;
         }else{
-            int amount = bill + enrollment;
-            System.out.println(amount);
+            amount = bill + enrollment;
         }
+        System.out.println(amount);
     }
 }
